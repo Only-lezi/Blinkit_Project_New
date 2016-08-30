a730acca5f8ccd982e33df7ce52f22332ba14b6f
Only-lezi <799795136@qq.com>
2016-08-30 13:24:28 +0800

    添加Constant，保存session的内容

---
+++ src/cn/blinkit/base/constant/Constant.java
@@ -0,0 +1,9 @@
+package cn.blinkit.base.constant;
+
+public class Constant {
+	// 系统中用户在session中的标识符
+	public static String USER = "currentUser";
+	// 系统中管理员在session中的标示符
+	public static String ADMIN = "currentAdmin";
+	
+}
